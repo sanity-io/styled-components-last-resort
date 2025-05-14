e50c9bd493628927e44a1604fda3d9276015cb2d
Cody Olsen <stipsan@gmail.com>
2025-05-14 16:58:34 +0200

    feat: improve example

---
+++ examples/styled-components/next-pages-router/src/pages/_app.tsx
@@ -0,0 +1,39 @@
+import type { AppProps } from 'next/app';
+import { buildTheme } from '@sanity/ui/theme';
+import { ThemeProvider } from '@sanity/ui';
+
+const theme = buildTheme();
+
+import { Inter } from 'next/font/google';
+import { createGlobalStyle } from 'styled-components';
+
+const inter = Inter({ subsets: ['latin'] });
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: ${inter.style.fontFamily};
+  }
+
+  html,
+  body {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  #__next {
+    display: contents;
+  }
+`;
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
--- examples/styled-components/next-pages-router/src/pages/_document.tsx
+++ examples/styled-components/next-pages-router/src/pages/_document.tsx
@@ -1,9 +1,9 @@
-import Document, { DocumentContext } from 'next/document';
-import React from 'react';
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+export default class NextDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
