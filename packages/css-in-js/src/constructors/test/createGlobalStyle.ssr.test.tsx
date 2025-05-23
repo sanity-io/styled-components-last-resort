a7180f17dcebe9b839d308c02152861dc934d72d
Cody Olsen <stipsan@gmail.com>
2025-05-23 16:52:52 +0200

    feat: add back `createGlobalStyle`

---
+++ packages/css-in-js/src/constructors/test/createGlobalStyle.ssr.test.tsx
@@ -0,0 +1,26 @@
+// @vitest-environment node
+import ReactDOMServer from 'react-dom/server';
+import createGlobalStyle from '../createGlobalStyle';
+
+describe(`createGlobalStyle`, () => {
+  let context: ReturnType<typeof setup>;
+
+  function setup() {
+    return {
+      renderToString(comp: React.JSX.Element) {
+        return ReactDOMServer.renderToString(comp);
+      },
+    };
+  }
+
+  beforeEach(() => {
+    context = setup();
+  });
+
+  it(`throws when rendered to string`, () => {
+    const Component = createGlobalStyle`[data-test-inject]{color:red;} `;
+    expect(() => context.renderToString(<Component />)).toThrowErrorMatchingInlineSnapshot(
+      `[TypeError: createGlobalStyle does not support SSR, use hydration check to detect it]`
+    );
+  });
+});
